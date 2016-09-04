bd77661c758217e07e10a2cc22e8bb1671abbe21
Sushant Gurung <tag35.sgurung@gmail.com>
2016-09-05 00:17:18 +0545

    Domain class.

--- grails-app/services/kfatyangra/RecommendService.groovy
+++ grails-app/services/kfatyangra/RecommendService.groovy
@@ -16,13 +16,10 @@ class RecommendService {
             insecticideGroup[insect?.insecticideId] = insect?.rating
         }
         userGroup[member?.id] = insecticideGroup
-
         def otherMember = Member.findAllByIdNotEqual(member?.id)
         otherMember.each{ itMember ->
             def insecticideGroup1 = [:]
-            def insecticides1 = PlantInsecticide.findAllByPlantAndMember(plant,itMember)?.insecticide
-            def insecticideList1 = Rating.findAllByInsecticideInListAndMember(insecticides,itMember)
-            println insecticideList1
+            def insecticideList1 = Rating.findAllByInsecticideInListAndMember(insecticides?.insecticide,itMember)
             if(insecticideList1){
                 insecticideList1.each{
                     insecticideGroup1[it?.insecticide?.id] = it?.rating
@@ -36,19 +33,23 @@ class RecommendService {
         def keys = userGroup1.keySet()
         def rList = []
         keys.each{
-            rList.add(correlationCalculator(userGroup,userGroup1[it],it))
+            rList.add(correlationCalculator(insecticideGroup,userGroup1[it],it))
+        }
+        rList.each{
+            println "------------------->" + it
         }
     }
     def correlationCalculator(user1, user2, user2Key){
+
         def keyList1 = user1?.keySet()
-        println keyList1
         def keyList2 = user2?.keySet()
-        println "-----" +keyList2
         def sameKey  = []
         if(keyList1 && keyList2){
-            keyList1.each{
-                keyList2.each{
-                    sameKey.add(keyList2)
+            keyList1.each{ k1->
+                keyList2.each{  k2 ->
+                    if(k1 == k2){
+                        sameKey.add(k1)
+                    }
                 }
             }
         }
@@ -61,17 +62,27 @@ class RecommendService {
             double sum2 = 0.00
             double sum2Sq = 0.00
             double pSum = 0.00
-            sameKey.each { same ->
-                println user1[same] +"--" +same
+            sameKey.each{ same->
+                println user1[same]+"--" +user2Key  +"----"+ user2[same]
                 sum1 += user1[same]
                 sum1Sq = sum1Sq +(user1[same].toString().toDouble() * user1[same].toString().toDouble())
                 sum2 += user2[same].toString().toDouble()
                 sum2Sq = sum2Sq +(user2[same].toString().toDouble() * user2[same].toString().toDouble())
                 pSum = pSum + (user1[same].toString().toDouble() * user2[same].toString().toDouble())
             }
+//            keys.each { same ->
+//                println same
+//                println user1.getAt('same') +"--" +user2Key  + user2.getAt('same')
+//                sum1 += user1[same]
+//                sum1Sq = sum1Sq +(user1[same].toString().toDouble() * user1[same].toString().toDouble())
+//                sum2 += user2[same].toString().toDouble()
+//                sum2Sq = sum2Sq +(user2[same].toString().toDouble() * user2[same].toString().toDouble())
+//                pSum = pSum + (user1[same].toString().toDouble() * user2[same].toString().toDouble())
+//            }
             def num=(pSum-(sum1*sum2/sameKey.size()))
             def den = Math.sqrt(((sum1Sq-(sum1 * sum1))*(sum2Sq-(sum2 * sum2))).doubleValue())
             if(den < 1 && den > -1){
+                println "eneter"
                 return 0.00
             }
 
